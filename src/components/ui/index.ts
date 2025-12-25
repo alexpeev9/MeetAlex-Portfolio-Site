aa6b6d9046a3b836401c6438011136e895b7fc12
alexpeev9 <alexpeev9@abv.bg>
2025-12-25 17:29:43 +0200

    Removed unused styles

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,5 @@
-export { default as Aurora } from "./Aurora";
 export { default as CountUp } from "./CountUp";
 export { default as FadeContent } from "./FadeContent";
-export { default as GlowingBorder } from "./GlowingBorder";
 export { default as SplitText } from "./SplitText";
-export { default as SpotlightCard } from "./SpotlightCard";
 export { default as Tag } from "./Tag";
 export { default as Text, textVariantStyles, type TextVariant } from "./Text";
